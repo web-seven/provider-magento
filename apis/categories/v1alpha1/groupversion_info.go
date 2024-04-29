d1b76298d7503bddc865f3a2d13b66173d60979e
Catalin <catalincretu1234@gmail.com>
2024-04-29 15:46:30 +0300

    generated Magento and Categories apis from crossplane template, added Magento and Categories api schema to schema builder and configured parameters of Category type

--- apis/categories/v1alpha1/groupversion_info.go
+++ apis/categories/v1alpha1/groupversion_info.go
@@ -14,9 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package v1alpha1 contains the v1alpha1 group Sample resources of the Template provider.
+// Package v1alpha1 contains the v1alpha1 group Sample resources of the Magento provider.
 // +kubebuilder:object:generate=true
-// +groupName=sample.template.crossplane.io
+// +groupName=categories.magento.crossplane.io
 // +versionName=v1alpha1
 package v1alpha1
 
@@ -27,7 +27,7 @@ import (
 
 // Package type metadata.
 const (
-	Group   = "sample.template.crossplane.io"
+	Group   = "categories.magento.crossplane.io"
 	Version = "v1alpha1"
 )
 
